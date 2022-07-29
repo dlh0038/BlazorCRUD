74e9dff3bd2033552316deabd92185ec69c4ea3c
Denny Hood <dhood@fbi.gov>
2022-07-29 07:21:29 -0400

    added rating and synopsis to movie entity

--- Shared/Models/Movie.cs
+++ Shared/Models/Movie.cs
@@ -11,5 +11,7 @@ namespace BlazorCRUD.Shared.Models
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        public string Rating { get; set; } = string.Empty; //added field
+        public string Synopsis {get; set; } = string.Empty;
     }
 }
